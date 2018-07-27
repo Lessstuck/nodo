db93e32143cb77dc9883d398969e0c7bf36eb743
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-07-27 00:16:38 +0000

    added express

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var foo = "foobar";
-console.log(foo);
+var express = require('express');
+var app = express();
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port);
+});
