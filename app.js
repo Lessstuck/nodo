29e9e4f53d64c4b4476360c146ad58cab603eeed
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-07-28 02:54:10 +0000

    install and configure bower

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var port = process.env.PORT;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
 
 app.listen(port, function(err){
     console.log('The server is running on port: ' + port);
