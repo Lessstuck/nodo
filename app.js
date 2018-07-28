a5f88df1504f1208a9749e5983b49fb1d6ca84c9
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-07-28 02:09:59 +0000

    conifgured static directories

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT;
 
-app.get('/', function (req, res) {
-    res.send('Hey hey ho');
-});
-
-
-app.get('/thang', function(req,res) {
-    res.send('thong');
-});
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.listen(port, function(err){
     console.log('The server is running on port: ' + port);
