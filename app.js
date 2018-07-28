7d91f67ff2b90ea708f8b693ae6eb3f2aab04ce1
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-07-28 01:12:35 +0000

    setting up template

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT;
+
+app.get('/', function (req, res) {
+    res.send('Hey hey hey');
+});
+
+
+app.get('/thang', function(req,res) {
+    res.send('thong');
+});
+
 app.listen(port, function(err){
     console.log('The server is running on port: ' + port);
 });
