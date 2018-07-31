0c98a69cca99d346992ac7d0e08a4872f2169708
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-07-31 21:12:09 +0000

    gulp for js, added override for bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,16 @@ var jsfiles = ['*.js', 'src/**/*.js'];
 gulp.task('style', function() {
     return gulp.src(jsfiles)
         .pipe(jshint());
-})
+});
+
+gulp.task('inject', function()  {
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './bower_components'
+    }
+    
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
+});
