e0802d03103d907d96d5dcf685228966585540cd
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-08-01 17:54:01 +0000

    added and configured gulp-inject

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,13 @@ gulp.task('style', function() {
 
 gulp.task('inject', function()  {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js']);
+    var injectOptions = {
+        ignorePath: '/public'
+    }
+    
     var options = {
         bowerJson: require('./bower.json'),
         directory: './bower_components',
@@ -18,5 +25,6 @@ gulp.task('inject', function()  {
     
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
