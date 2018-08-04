8d0d2e60c253a6fd7d05938b1c5eb72ea07ee4c9
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-08-04 01:37:29 +0000

    converted nav menu to EJS

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index', { list: [ 'first val', '2nd val', '3rd val' ],
+    nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+    })});
 
 app.listen(port, function(err){
     console.log('The server is running on port: ' + port);
