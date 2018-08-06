3c94c4587507ba98ab4987ab044f248f284c4b19
EC2 Default User <ec2-user@ip-172-31-24-14.us-east-2.compute.internal>
2018-08-06 20:34:07 +0000

    added eventRouter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT;
+var eventRouter = express.Router();
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
@@ -8,9 +9,27 @@ app.use(express.static('bower_components'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+eventRouter.route('/')
+    .get(function(req, res) {
+        res.send('Hello Events');
+    })
+    
+eventRouter.route('/event')
+    .get(function(req, res) {
+        res.send('Hello Event');
+    })
+    
+    
+app.use('/Events', eventRouter)    
+
 app.get('/', function(req, res) {
     res.render('index', { list: [ 'first val', '2nd val', '3rd val' ],
-    nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+    nav: [{Link : 'Services', Text : 'Services'},
+          {Link : 'Portfolio', Text : 'Portfolio'},
+          {Link : 'About', Text : 'About'},
+          {Link : 'Team', Text : 'Team'},
+          {Link : 'Contact', Text : 'Contact'},
+          {Link : 'Events', Text : 'Events'}]
     })});
 
 app.listen(port, function(err){
